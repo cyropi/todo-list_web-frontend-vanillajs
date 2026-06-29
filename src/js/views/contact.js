4aa6b91e0ea7be59d0bebcd52f37d102151dd7b2
Ciro Pizza <ciropizza2002@gmail.com>
2026-06-29 18:29:08 +0200

    Update contact.js

--- src/js/views/contact.js
+++ src/js/views/contact.js
@@ -1,1 +1,9 @@
 
+export default () => ` <h1>
+                           Contact
+                       </h1>
+                         
+                       <p>
+                           Mail: ciropizza2002@gmail.com
+                       </p>
+                     `;
