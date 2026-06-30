04704912eb78f2cec1dd8a861fddf0ed7b1cd980
Ciro Pizza <ciropizza2002@gmail.com>
2026-06-30 19:02:14 +0200

    Update login.js

--- src/js/views/login.js
+++ src/js/views/login.js
@@ -1,1 +1,21 @@
 
+export default () => {
+                         const urlParams = new URLSearchParams(window.location.search);
+                         const hasError = urlParams.get("error");
+
+                         const bannerHTML = hasError 
+                                          ? ` <div style="background-color: #ffcccc; 
+                                                          color: #cc0000; 
+                                                          padding: 10px; 
+                                                          margin-bottom: 15px; 
+                                                          border-radius: 5px; 
+                                                          text-align: center;">
+                                                  <b>Access Denied!</b> 
+                                                  Please login to view this page.
+                                              </div>`
+                                          : '';
+
+                        return ` ${bannerHTML}
+                                <login-form></login-form>
+                               `;
+                     }
