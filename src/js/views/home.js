0b6cb1e871c07f2849547a221f8d27e0e7463e14
Ciro Pizza <ciropizza2002@gmail.com>
2026-06-30 19:01:39 +0200

    Update home.js

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,1 +1,17 @@
 
+export default () => ` <h1>
+                           To-do List SPA
+                       </h1>
+                       
+                       <p>
+                           Our beloved To-do list web app, now as a Single Page Application (SPA)
+                           implemented using plain JavaScript and custom Web Components.
+                       </p>
+                       
+                        <p>
+                           This app is meant as a didactic example to showcase SPAs within the 
+                           Web Technologies course at the University of Naples Federico II. 
+                           <br>
+                           It is affected by performance issues.
+                       </p>   
+                     `;
